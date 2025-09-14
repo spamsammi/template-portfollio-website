de8432ef87b67b81b19234e1950463ab145dc5fd
Luke Henry <luke.d.henry1@gmail.com>
2025-09-14 17:03:55 -0500

    inital svelte kit creation and json config added

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Header from './Header.svelte';
+</script>
+
+<Header />
+
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
---
+++ src/routes/Header.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import config from '$lib/config.json';
+</script>
+
+<header>
+    <h1>{config.title}</h1>
+</header>
