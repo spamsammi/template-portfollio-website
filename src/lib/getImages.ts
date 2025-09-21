8ebedf161b5816512f4626e409220f046639132d
Luke Henry <luke.d.henry1@gmail.com>
2025-09-21 11:33:28 -0500

    added styling

--- src/lib/getImages.ts
+++ src/lib/getImages.ts
@@ -1,5 +1,8 @@
-// Utility functions for image imports using Vite's import.meta.glob
-
+/* Vite cannot have variables for glob pattern, so to use this function, you will to get images like so:
+* Example:
+*  const images import.meta.glob('../images/logo/*.{jpg,png,svg}', { eager: true }) as Record<string, { default: string }>;
+* Then pass the images to this function.
+*/
 export function getImageByNameOrFirst(images: Record<string, { default: string }>, name?: string): string | undefined {
 if (name) {
     const entry = Object.entries(images).find(([path]) => path.includes(name));
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -10,7 +10,7 @@
   <div class="header">
     <div class="header-left">
       <a href="/" class="logo"><img src={logo} alt="Logo"></a>
-      <h2>{config.title.text}</h2>
+      <h1>{config.title.text}</h1>
     </div>
     <div class="header-right">
       {#each config.header.links as link}
@@ -22,12 +22,14 @@
 
 <style>
   .header {
+    background-color: var(--color-pallete-1);
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 1rem 2rem;
   }
   .header-left {
+    color: var(--color-pallete-7);
     display: flex;
     align-items: center;
     gap: 1rem;
@@ -36,8 +38,8 @@
     width: 3rem;
     height: auto;
   }
-
-  .header-left h2 {
+  .header-left h1 {
+    font-family: var(--font-title);
     margin: 0;
   }
   .header-right {
@@ -46,7 +48,15 @@
     gap: 1.5rem;
   }
   .header-right a {
+    color: var(--color-pallete-7);
+    font-family: var(--font-header);
+    font-size: 1.2rem;
     text-decoration: none;
+    transition: transform 0.3s cubic-bezier(0.4, 0, 0.2, 1); /* Smooth, gradual */
+  }
+  .header-right a:hover {
+    color: var(--color-pallete-5);
+    transform: scale(1.15);
   }
 </style>
   
